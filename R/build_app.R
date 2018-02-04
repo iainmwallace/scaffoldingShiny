c1e40b771f1b99c6ff1439511a5c97878ea6ccdd
Wallace, Iain <iain.wallace@merck.com>
2018-02-04 14:36:06 -0800

    Functions to write shiny required files (md + global)

--- R/build_app.R
+++ R/build_app.R
@@ -5,6 +5,10 @@ build_basic_app<-function(input_list,data_file="my_file.csv"){
 
   create_ui_file(input_list)
   create_server_file(input_list,save_path = data_file)
+  write_save_data_info_file()
+  write_browse_data_info_file()
+  write_global_file()
+
   shiny::runApp()
 
 }
