88e563bd92fc43631ecf667d779f02b3815fd1d8
Wallace, Iain <iain.wallace@merck.com>
2018-02-04 14:36:06 -0800

    Functions to write shiny required files (md + global)

---
+++ R/files_for_shiny.R
@@ -0,0 +1,30 @@
+
+write_global_file<-function(file_name='global.r'){
+  x<-"library(shiny)
+library(bsplus)
+  library(shinydashboard)
+  library(shinycssloaders)
+  library(shinyjs)
+
+  # for server functions
+  library(dplyr)
+  library(DT)
+  library(glue)
+  library(readr)"
+
+  writeLines(file_name)
+}
+
+write_save_data_info_file<-function(file_name='save_data.md'){
+  x<-"# Save data form
+Details should go here
+  "
+  writeLines(file_name)
+
+}
+
+write_browse_data_info_file<-function(file_name='browse_data.md'){
+  x<-"# Browse data form
+Details should go here"
+
+}
