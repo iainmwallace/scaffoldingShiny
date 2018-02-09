c5f12b328581ecd1cbfd783047b38f2cabf6d306
Wallace, Iain <iain.wallace@merck.com>
2018-02-09 16:22:27 -0500

    Added ability to use sqllite as backend, automatic formating of generated files, additional input types

--- tests/testthat/server.r
+++ tests/testthat/server.r
@@ -1,50 +1,51 @@
 function(input, output, session) {
-
-dataset_to_save<-reactive({
-my_data <- tibble(
-id1=input$id1,
-id2=input$id2)
- return(my_data)})
-
-registerData<-observeEvent(input$save,{
-
-  my_dataset<-dataset_to_save()
-
-  withProgress(message="Registering",value=0.2,{
-if(file.size("my_file.csv")>10){
-            write_csv(my_dataset,path="my_file.csv",append = TRUE)
-}else{
-            write_csv(my_dataset,path="my_file.csv",append = FALSE)
-}
-}) 
- showModal(modalDialog(
-
-  title = "Thank you!",
-  "Data has been saved"
-  ))
+  dataset_to_save <- reactive({
+    my_data <- tibble(
+      id1 = input$id1,
+      id2 = input$id2
+    )
+    return(my_data)
   })
-allData <- reactiveFileReader(1000, session,"my_file.csv","read_csv")
-output$data <- DT::renderDataTable({
 
-  data<-allData()
+  registerData <- observeEvent(input$save, {
+    my_dataset <- dataset_to_save()
 
-  data<-data%>%select(
-  everything())
-
-  DT::datatable(data,  selection = 'single',
-  filter='top',
-  rownames= FALSE,
-  escape=FALSE,
-  options = list(
-  scrollX = TRUE)
-  )
-})
-output$download_data <- downloadHandler(
-  filename = function() {
-  paste("download_data", ".csv", sep = "")
-  },
-  content = function(file) {
-  write_csv(allData(), file)
+    withProgress(message = "Registering", value = 0.2, {
+      dbWriteTable(conn, datatable, my_dataset, append = TRUE)
+    })
+    showModal(modalDialog(
+      title = "Thank you!",
+      "Data has been saved"
+    ))
+  })
+  allData <- function() {
+    my_data <- tbl(conn, datatable) %>% collect()
+    shiny::validate(need(nrow(my_data) > 0, "no data"))
+    return(my_data)
   }
+  output$data <- DT::renderDataTable({
+    data <- allData()
+
+    data <- data %>% select(
+      everything()
+    )
+
+    DT::datatable(
+      data, selection = "single",
+      filter = "top",
+      rownames = FALSE,
+      escape = FALSE,
+      options = list(
+        scrollX = TRUE
+      )
+    )
+  })
+  output$download_data <- downloadHandler(
+    filename = function() {
+      paste("download_data", ".csv", sep = "")
+    },
+    content = function(file) {
+      write_csv(allData(), file)
+    }
   )
 }
--- tests/testthat/ui.r
+++ tests/testthat/ui.r
@@ -1,66 +1,73 @@
 
 # input -------------------------------------------------------------------
-  
-input_data_tab<-function(){
-  tabItem(tabName = "input_data_tab",
-  fluidRow(
-  box(width=12,title="Details",
-  includeMarkdown("save_data.md"))
-  ),
-  fluidRow(
-  box(width=12,title="Input Data",
-textInput(inputId="id1",label="new_blah1",placeholder="Change placeholder"),
-textInput(inputId="id2",label="blah2",placeholder="Change placeholder"))
-  ),fluidRow(
-  actionButton("save", "Save")
 
+input_data_tab <- function() {
+  tabItem(
+    tabName = "input_data_tab",
+    fluidRow(
+      box(
+        width = 12, title = "Details",
+        includeMarkdown("save_data.md")
+      )
+    ),
+    fluidRow(
+      box(
+        width = 12, title = "Input Data",
+        numericInput(inputId = "id1", label = "new_blah1", value = 1, min = NA, max = NA, step = NA)
+        ,
+        selectInput(inputId = "id2", label = "blah2", choices = c("change", "here"), selected = "", multiple = TRUE)
+      )
+    ), fluidRow(
+      actionButton("save", "Save")
+    )
   )
-  )
-  }
+}
 
 # browse tab function ------------------------------------------------------------------
-  
-browse_data_tab<-function(){
-  tabItem(tabName = "browse_data_tab",
-  fluidRow(
-  box(width=12,title="Details",
-  includeMarkdown("browse_data.md"))
-  ),
-  fluidRow(
-  box(width=12,title="Browse Data",
-  withSpinner(DT::dataTableOutput('data')),
-  downloadButton("download_data")
-  )
-  )
 
+browse_data_tab <- function() {
+  tabItem(
+    tabName = "browse_data_tab",
+    fluidRow(
+      box(
+        width = 12, title = "Details",
+        includeMarkdown("browse_data.md")
+      )
+    ),
+    fluidRow(
+      box(
+        width = 12, title = "Browse Data",
+        withSpinner(DT::dataTableOutput("data")),
+        downloadButton("download_data")
+      )
+    )
   )
-  }
+}
 
 # Dashboard ---------------------------------------------------------------
-  
+
 dashboardPage(
   dashboardHeader(title = "Title"),
   dashboardSidebar(
-  sidebarMenu(
-  id = "tabs",
-  menuItem("Input data", tabName = "input_data_tab", icon=icon("info")),
-  menuItem("Browse data",tabName = "browse_data_tab",icon = icon("info"))
-  )
-
+    sidebarMenu(
+      id = "tabs",
+      menuItem("Input data", tabName = "input_data_tab", icon = icon("info")),
+      menuItem("Browse data", tabName = "browse_data_tab", icon = icon("info"))
+    )
   ),
   dashboardBody(
-  #tags$head(includeScript("tracking.js")),
-  useShinyjs(),
-  #    shinyjs::inlineCSS(appCSS),
-  tags$head(tags$style(HTML("
+    # tags$head(includeScript("tracking.js")),
+    useShinyjs(),
+    #    shinyjs::inlineCSS(appCSS),
+    tags$head(tags$style(HTML("
   .shiny-split-layout > div {
   overflow: visible;
   }
   "))),
-  tabItems(
-  input_data_tab(),
-  browse_data_tab()
-  ),
-  use_bs_tooltip()
+    tabItems(
+      input_data_tab(),
+      browse_data_tab()
+    ),
+    use_bs_tooltip()
   )
-  )
+)
